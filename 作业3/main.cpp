3a823d1df18a35da243ac2459515231f6c7b7b5f
lucaslee0921 <114377621+lucaslee0921@users.noreply.github.com>
2022-10-28 21:14:41 +0800

    Update main.cpp

--- 作业3/main.cpp
+++ 作业3/main.cpp
@@ -23,15 +23,19 @@ void testprogram()
      double total;
 
      start = clock();
+ 
      cout << "程序启动，start = " << start ;
 
      cout << "开始循环，start = " << start ;
 
      end = clock();
+     
      cout << "循环结束，end = " << end ;
 
-     total = (double)(end - start) / CLOCKS_PER_SEC;
-     cout << "CPU 占用的总时间：" << total  );
+     total = (double)(end - start) / CLOCKS_PER_SEC ;
+ 
+     cout << "CPU 占用的总时间：" << total  ;
+ 
      cout << "程序退出..." << endl ;
 
      return(0);
